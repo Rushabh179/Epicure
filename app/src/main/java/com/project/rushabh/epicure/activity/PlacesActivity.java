cf33d5b8a3da19d2767a71623ca601a7138a2c51
Rushabh Modi <modi.rushabh179@gmail.com>
2018-03-06 00:44:42 +0530

    Places recycler list

--- app/src/main/java/com/project/rushabh/epicure/activity/PlacesActivity.java
+++ app/src/main/java/com/project/rushabh/epicure/activity/PlacesActivity.java
@@ -3,16 +3,22 @@ package com.project.rushabh.epicure.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
 import com.project.rushabh.epicure.R;
 
-public class MainActivity extends AppCompatActivity {
+public class PlacesActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerview_main);
     }
 
     public void toItems(View view) {
---
+++ app/src/main/java/com/project/rushabh/epicure/adapter/PlacesRecyclerViewAdapter.java
@@ -0,0 +1,34 @@
+package com.project.rushabh.epicure.adapter;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Rushabh on 05-Mar-18.
+ */
+
+public class PlacesRecyclerViewAdapter extends RecyclerView.Adapter<PlacesRecyclerViewAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
