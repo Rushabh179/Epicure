0e2bda2f9945928656b1140533991a47fe1abf28
Rushabh Modi <modi.rushabh179@gmail.com>
2018-03-22 19:04:25 +0530

    Update

---
+++ app/src/main/java/com/project/rushabh/epicure/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.project.rushabh.epicure.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.project.rushabh.epicure.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
