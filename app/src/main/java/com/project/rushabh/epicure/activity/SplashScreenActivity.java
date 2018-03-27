74addcc7e24e86a826281ac536264450ea8fe07b
Rushabh Modi <modi.rushabh179@gmail.com>
2018-03-27 12:50:04 +0530

    Implemented splash activity

--- app/src/main/java/com/project/rushabh/epicure/activity/SplashScreenActivity.java
+++ app/src/main/java/com/project/rushabh/epicure/activity/SplashScreenActivity.java
@@ -6,12 +6,22 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.project.rushabh.epicure.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     SharedPreferences sharedPreferences;
+    private Button toSignUpBtn, toLogInButton, toMainBtn;
+    private TextView mainTitleText;
+    private ImageView mainLogoImage;
+    private Animation fadeInAnim, elevateAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +31,38 @@ public class SplashScreenActivity extends AppCompatActivity {
         if (getSupportActionBar() != null)
             getSupportActionBar().hide();
 
+        toSignUpBtn = findViewById(R.id.btn_to_signup);
+        toLogInButton = findViewById(R.id.btn_to_login);
+        toMainBtn = findViewById(R.id.btn_to_main);
+
+        mainTitleText = findViewById(R.id.text_title_main);
+        mainLogoImage = findViewById(R.id.image_main_logo);
+
+        fadeInAnim = AnimationUtils.loadAnimation(this, R.anim.anim_fadein);
+        elevateAnim = AnimationUtils.loadAnimation(this, R.anim.anim_elevate);
+
         sharedPreferences = getApplicationContext().getSharedPreferences(getString(R.string.shared_pref_file_name), MODE_PRIVATE);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        /*if (!sharedPreferences.getBoolean(getString(R.string.is_logged_in), false)){
-            finish();
-            startActivity(new Intent(this, LoginActivity.class));
+        if (!sharedPreferences.getBoolean(getString(R.string.is_logged_in), false)) {
+            //Toast.makeText(this, "You're not logged in", Toast.LENGTH_SHORT).show();
+            mainLogoImage.startAnimation(elevateAnim);
+            mainTitleText.startAnimation(elevateAnim);
+            toSignUpBtn.startAnimation(fadeInAnim);
+            toLogInButton.startAnimation(fadeInAnim);
+            toMainBtn.startAnimation(fadeInAnim);
         } else {
-            finish();
+            /*mainTitleText.startAnimation(steadyAnim);
+            mainLogoImage.startAnimation(steadyAnim);
+            toSignUpBtn.setVisibility(View.INVISIBLE);
+            toLogInButton.setVisibility(View.INVISIBLE);
+            toMainBtn.setVisibility(View.INVISIBLE);*/
             startActivity(new Intent(this, MainActivity.class));
-        }*/
+            finish();
+        }
     }
 
     public void onSplashClick(View view) {
@@ -43,7 +72,7 @@ public class SplashScreenActivity extends AppCompatActivity {
             case R.id.btn_to_login:
                 startActivity(new Intent(this, LoginActivity.class));
                 break;
-            case R.id.btn_without_login:
+            case R.id.btn_to_main:
                 finish();
                 startActivity(new Intent(this, MainActivity.class));
         }
