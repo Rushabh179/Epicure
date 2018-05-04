edc60e4eab0a3235cada612d71dc10eb5217bc18
Rushabh Modi <modi.rushabh179@gmail.com>
2018-05-05 01:08:34 +0530

    Restaurant info

--- app/src/main/java/com/project/rushabh/epicure/adapter/PlacesRecyclerViewAdapter.java
+++ app/src/main/java/com/project/rushabh/epicure/adapter/PlacesRecyclerViewAdapter.java
@@ -1,8 +1,11 @@
 package com.project.rushabh.epicure.adapter;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +15,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.GeoPoint;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.project.rushabh.epicure.R;
 import com.project.rushabh.epicure.activity.MapsActivity;
 import com.project.rushabh.epicure.interfaces.OnRecyclerClickListener;
@@ -30,6 +38,9 @@ public class PlacesRecyclerViewAdapter extends RecyclerView.Adapter<PlacesRecycl
     private List<GeoPoint> placesLocationList;
     private Context context;
     private OnRecyclerClickListener onRecyclerClickListener;
+    private View dialogView;
+    private TextView emailText, nameText, phoneText, addressText;
+    private FirebaseFirestore db;
 
     public PlacesRecyclerViewAdapter(Context context, List<List<String>> placesLists, List<GeoPoint> placesLocationList) {
         this.context = context;
@@ -54,7 +65,7 @@ public class PlacesRecyclerViewAdapter extends RecyclerView.Adapter<PlacesRecycl
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.placeTitleText.setText(placesNameList.get(position));
+        holder.placesTitleText.setText(placesNameList.get(position));
         holder.placesDetailsText.setText(placesInformationList.get(position));
         Glide.with(context).load(placesImageList.get(position)).into(holder.placesThumbImage);
     }
@@ -66,17 +77,18 @@ public class PlacesRecyclerViewAdapter extends RecyclerView.Adapter<PlacesRecycl
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
-        private TextView placeTitleText, placesDetailsText;
+        private TextView placesTitleText, placesDetailsText;
         private ImageView placesThumbImage, placesLocationImage;
 
         public ViewHolder(final View itemView) {
             super(itemView);
-            placeTitleText = itemView.findViewById(R.id.text_places_title);
+            placesTitleText = itemView.findViewById(R.id.text_places_title);
             placesDetailsText = itemView.findViewById(R.id.text_places_details);
             placesThumbImage = itemView.findViewById(R.id.image_places_thumb);
             placesLocationImage = itemView.findViewById(R.id.image_place_location);
+            db = FirebaseFirestore.getInstance();
 
-            placeTitleText.setOnClickListener(new View.OnClickListener() {
+            placesTitleText.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     Toast.makeText(context, Integer.toString(getAdapterPosition()), Toast.LENGTH_SHORT).show();
@@ -96,6 +108,22 @@ public class PlacesRecyclerViewAdapter extends RecyclerView.Adapter<PlacesRecycl
                 }
             });
 
+            placesTitleText.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    showInfo(getAdapterPosition());
+                    return false;
+                }
+            });
+
+            placesDetailsText.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    showInfo(getAdapterPosition());
+                    return false;
+                }
+            });
+
             placesLocationImage.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -107,4 +135,42 @@ public class PlacesRecyclerViewAdapter extends RecyclerView.Adapter<PlacesRecycl
             });
         }
     }
+
+    @SuppressLint("InflateParams")
+    public void showInfo(int position){
+        LayoutInflater inflater = LayoutInflater.from(context);
+        dialogView = inflater.inflate(R.layout.dialog_places_info, null, false);
+        emailText = dialogView.findViewById(R.id.editText_dialog_email);
+        nameText = dialogView.findViewById(R.id.editText_dialog_name);
+        phoneText = dialogView.findViewById(R.id.editText_dialog_phone);
+        addressText = dialogView.findViewById(R.id.editText_dialog_address);
+
+        db.collection("restaurants").whereEqualTo("name", placesNameList.get(position))
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                emailText.setText(document.getString("email"));
+                                nameText.setText(document.getString("name"));
+                                phoneText.setText(document.getString("contact"));
+                                addressText.setText(document.getString("address"));
+                            }
+                        }
+                    }
+                });
+        new AlertDialog.Builder(context)
+                .setTitle("Restaurant Info")
+                .setView(dialogView)
+                .setNeutralButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                })
+                .create()
+                .show();
+    }
+
 }
